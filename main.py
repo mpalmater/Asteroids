ea670d73156aafd1b0e58dfc388d6374022acd4a
Mitchell Palmater <mitchellpalmater@Mitchells-MacBook-Pro.local>
2025-11-21 12:12:40 -0400

    not working

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 
 print(f"Starting Asteroids with version: {pygame.version.ver}")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        log_state()
+        log_state(player)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 main()
